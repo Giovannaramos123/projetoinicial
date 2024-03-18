77f30569875fc65d29e6f8b799174f6cfdc8fd9c
Giovannaramos123 <gigi.sk.moon@gmail.com>
2024-03-18 08:26:31 -0300

    Formulário

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 var botao = document.getElementById("botao")
 botao.addEventListener("click", alertar);
 
+var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
+var compl = document.getElementById("compl");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
+
 function alertar(){
     alert("O botão foi clicado!!!");
+    saida.innerText = "Nome: " + nome.value + "\n Email: " + email.value + "\n Telefone: " + telefone.value + "\n CEP: " + cep.value
+    + "\n Logradouro: " + logradouro.value + "\n Número da casa: " + numero.value + "\n Complemento: " + compl.value + "\n Bairro: " + bairro.value
+    + "\n Cidade: " + cidade.value + "\n Estado: " + estado.value;
 }
 
